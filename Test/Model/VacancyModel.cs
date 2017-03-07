77196e2cb2cbc538bff76e527c01716d1bfbf6a9
pmishin <mishinp40@gmail.com>
2017-03-07 21:23:10 +0300

    add repository

---
+++ Test/Model/VacancyModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using hhModel;
+
+namespace Test.Model
+{
+	public class VacancyModel
+	{
+		private string _description;
+		public string Id { get; set; }
+		public string Alternate_Url { get; set; }
+		public string Description
+		{
+			get { return _description; }
+			set { _description = StripHtmlTagsUsingRegex(value); }
+		}
+		public string Name { get; set; }
+		public Experience Experience { get; set; }
+		public Area Area { get; set; }
+		public Salary Salary { get; set; }
+		public Schedule Schedule { get; set; }
+		static string StripHtmlTagsUsingRegex(string inputString)
+		{
+			return Regex.Replace(inputString, @"<[^>]*>", String.Empty).Replace("&quot;", "\"").Trim();
+		}
+
+		public static List<string> GetVacancyList()
+		{
+			List<string> items = new List<string>();
+			using (WebClient webClient = new WebClient())
+			{
+				webClient.Encoding = Encoding.UTF8;
+				webClient.Headers.Add(HttpRequestHeader.UserAgent, "test-app");
+				string json = webClient.DownloadString(ConfigurationManager.AppSettings["RequestURL"]);
+				JsonConvert.DeserializeObject<RootObject>(json).items.ForEach(o => items.Add(o.id));
+
+			}
+			return items;
+		}
+
+		public static async Task<List<VacancyModel>> GetVacancyInfoAsync(List<string> urlList)
+		{
+			return await Task<List<VacancyModel>>.Factory.StartNew(() =>
+			{
+				List<VacancyModel> resList = new List<VacancyModel>();
+				List<string> temp = new List<string>();
+				foreach (var item in urlList)
+				{
+					using (WebClient wc = new WebClient())
+					{
+						wc.Encoding = Encoding.UTF8;
+						wc.Headers.Add(HttpRequestHeader.UserAgent, "test-app");
+						temp.Add(wc.DownloadString(new Uri("https://api.hh.ru/vacancies/" + item)));
+					}
+				}
+				temp.ForEach(o => resList.Add(JsonConvert.DeserializeObject<VacancyModel>(o)));
+				return resList;
+			});
+		}
+
+	}
+
+	public class Salary
+	{
+		public int _from { get; set; }
+		public int _to { get; set; }
+		public int? From { get { return _from; } set { _from = value ?? 0; } }
+		public int? To { get { return _to; } set { _to = value ?? 0; } }
+		public string TotalSalary { get
+		{
+			return From != 0 && To != 0 ? $"ОТ {From} ДО {To} {Currency}":"Не указано";
+		
+		} }
+		public string Currency { get; set; }
+	}
+	public class Schedule
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+	}
+	public class Experience
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
+	}
+	public class Area
+	{
+		public string Url { get; set; }
+		public string Id { get; set; }
+		public string Name { get; set; }
+	}
+}
+
---
+++ Test/ViewModel/MainViewModel.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Data;
+using System.Windows.Input;
+using Test.Model;
+
+namespace Test.ViewModel
+{
+	public class MainViewModel : INotifyPropertyChanged
+	{
+		private ICollectionView vacanciesList;
+		private VacancyModel selectedVacancy;
+		private string searchString;
+		private ObservableCollection<VacancyModel> tabList;
+		private VacancyModel selectedTab;
+		private ICommand closeTabCommand;
+		private ICommand navigateURL;
+
+		
+		public ICollectionView VacanciesList
+		{
+			get { return vacanciesList; }
+			set
+			{
+				vacanciesList = value;
+				OnPropertyChanged();
+			}
+		}
+		public VacancyModel SelectedVacancy
+		{
+			get { return selectedVacancy; }
+			set
+			{
+				selectedVacancy = value;
+
+				if (value != null && !TabList.Contains(value))
+				{
+					TabList.Add(value);
+					SelectedTab = value;
+				}
+				OnPropertyChanged();
+
+			}
+		}
+		public string SearchString
+		{
+			get { return searchString; }
+			set
+			{
+				searchString = value;
+				OnPropertyChanged();
+				if (vacanciesList != null)
+				{
+					VacanciesList.Filter = null;
+					VacanciesList.Filter = Filter;
+				}
+			}
+		}
+		public ObservableCollection<VacancyModel> TabList
+		{
+			get { return tabList ?? new ObservableCollection<VacancyModel>(); }
+			set
+			{
+				tabList = value;
+			}
+		}
+		public VacancyModel SelectedTab
+		{
+			get { return selectedTab; }
+			set
+			{
+				selectedTab = value;
+				OnPropertyChanged();
+			}
+		}
+		public ICommand CloseTabCommand
+		{
+			get
+			{
+				return closeTabCommand;
+			}
+		}
+		public ICommand NavigateUrlCommand
+		{
+			get
+			{
+				return navigateURL;
+			}
+		}
+
+
+		public event PropertyChangedEventHandler PropertyChanged;
+		public void OnPropertyChanged([CallerMemberName]string prop = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+		}
+		private bool Filter(object o)
+		{
+			bool result = true;
+			var model = o as VacancyModel;
+			if (model != null && !string.IsNullOrWhiteSpace(SearchString) && !model.Name.ToLower().Contains(SearchString.ToLower()))
+				result = false;
+			return result;
+		}
+		private async void InitilizeVacanciesList()
+		{
+			this.VacanciesList = CollectionViewSource.GetDefaultView(await VacancyModel.GetVacancyInfoAsync(VacancyModel.GetVacancyList()));
+		}
+
+		public MainViewModel()
+		{
+			InitilizeVacanciesList();
+			TabList = new ObservableCollection<VacancyModel>();
+			closeTabCommand = new RelayCommand(o => {
+					TabList.Remove(TabList.FirstOrDefault(x => x.Id == (o as string)));
+					SelectedVacancy = null;
+				}
+				,(obj) => true);
+			navigateURL = new RelayCommand(o => System.Diagnostics.Process.Start(o.ToString()), (obj) => true);
+		}
+	}
+	public class RelayCommand : ICommand
+	{
+		private Action<object> execute;
+		private Func<object, bool> canExecute;
+
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+		{
+			this.execute = execute;
+			this.canExecute = canExecute;
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return this.canExecute == null || this.canExecute(parameter);
+		}
+
+		public void Execute(object parameter)
+		{
+			this.execute(parameter);
+		}
+	}
+}
